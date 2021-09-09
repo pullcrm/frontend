a26033f62987e3a598e8758a1619c65275fb7d9a
tsarrB <bogdan.tsarik@gmail.com>
2021-09-09 20:05:08 +0300

    Update meta viewport value

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -73,7 +73,7 @@ const config: NuxtOptions = {
     title: 'Учет клиентов и онлайн запись - pullcrm',
     meta: [
       { charset: 'utf-8' },
-      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover' },
+      { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover' },
       { name: 'mobile-web-app-capable', content: 'yes' },
       { name: 'apple-mobile-web-app-capable', content: 'yes' },
       { name: 'application-name', content: 'Pullcrm' },
